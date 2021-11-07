0250059c321a839e8e7921d4f4f34bb151d1572f
Bonyeong1998 <bonyoung1998@gmail.com>
2021-11-07 23:31:43 +0900

    rename

--- client/app/src/main/java/com/example/myapplication/owner/OwnerMain.java
+++ client/app/src/main/java/com/example/myapplication/owner/OwnerMain.java
@@ -5,13 +5,12 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
-import com.example.myapplication.doain.User;
 
 public class OwnerMain extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.owner_main); //메인 화면
+        setContentView(R.layout.owner_seat_manage); //메인 화면
     }
 }
