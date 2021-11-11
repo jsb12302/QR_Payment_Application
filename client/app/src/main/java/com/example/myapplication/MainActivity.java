beae871fd8dc94ae3f41e2cf06fceec19ab87e04
Bonyeong1998 <bonyoung1998@gmail.com>
2021-11-12 04:20:55 +0900

    좌석관리화면

--- client/app/src/main/java/com/example/myapplication/MainActivity.java
+++ client/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,81 +25,81 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    RadioGroup radioGroup;
-    EditText IdText;
-    EditText PwdText;
-    Role loginUserType = Role.ROLE_OWNER;
-    private String loginId;
-    private String loginPwd;
+//    RadioGroup radioGroup;
+//    EditText IdText;
+//    EditText PwdText;
+//    Role loginUserType = Role.ROLE_OWNER;
+//    private String loginId;
+//    private String loginPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_screen); //메인 화면
+        setContentView(R.layout.owner_seat_manage); //메인 화면
 
-        IdText = (EditText) findViewById(R.id.login_Id);
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-                switch (checkedId){
-                    case R.id.owner_choice:
-                        IdText.setHint("가맹점주 아이디 입력");
-                        loginUserType = Role.ROLE_OWNER;
-                        break;
-                    case R.id.user_choice:
-                        IdText.setHint("일반사용자 아이디 입력");
-                        loginUserType = Role.ROLE_USER;
-                        break;
-                }
-            }
-        });
+//        IdText = (EditText) findViewById(R.id.login_Id);
+//        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+//
+//        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+//                switch (checkedId){
+//                    case R.id.owner_choice:
+//                        IdText.setHint("가맹점주 아이디 입력");
+//                        loginUserType = Role.ROLE_OWNER;
+//                        break;
+//                    case R.id.user_choice:
+//                        IdText.setHint("일반사용자 아이디 입력");
+//                        loginUserType = Role.ROLE_USER;
+//                        break;
+//                }
+//            }
+//        });
     }
-
-    public void go_login(View v) throws JSONException {
-
-        IdText = (EditText) findViewById(R.id.login_Id);
-        PwdText = (EditText) findViewById(R.id.login_Pwd);
-
-        loginId = IdText.getText().toString();
-        loginPwd = PwdText.getText().toString();
-
-        new Thread(new ConnectRunner()).start();
-    }
-
-    public class ConnectRunner implements Runnable {
-
-        @Override
-        public void run() {
-            HttpService httpService = HttpClient.getApiService();
-
-            LoginRequestDto loginRequestDto = new LoginRequestDto(loginId, loginPwd, loginUserType);
-            System.out.println(loginUserType);
-
-            try {
-                Response<Message> loginResponse = httpService.login(loginRequestDto).execute();
-                Message message=loginResponse.body();
-                if (message.getRole()== Role.ROLE_OWNER){
-                    Intent intent = new Intent(getApplicationContext(), OwnerMain.class);
-                    startActivity(intent);
-                }
-                else if (message.getRole()== Role.ROLE_USER){
-                    Intent intent = new Intent(getApplicationContext(), UserMain.class);
-                    startActivity(intent);
-                }
-            }catch (Exception e){
-                System.out.println("오류");
-            }
-        }
-    }
-
-    public void move_login(View v){
-        Intent intent = new Intent(getApplicationContext(), Login.class);
-        startActivity(intent);
-    }
-    public void go_signup(View v){
-        Intent intent = new Intent(getApplicationContext(), SignUp.class);
-        startActivity(intent);
-    }
+//
+//    public void go_login(View v) throws JSONException {
+//
+//        IdText = (EditText) findViewById(R.id.login_Id);
+//        PwdText = (EditText) findViewById(R.id.login_Pwd);
+//
+//        loginId = IdText.getText().toString();
+//        loginPwd = PwdText.getText().toString();
+//
+//        new Thread(new ConnectRunner()).start();
+//    }
+//
+//    public class ConnectRunner implements Runnable {
+//
+//        @Override
+//        public void run() {
+//            HttpService httpService = HttpClient.getApiService();
+//
+//            LoginRequestDto loginRequestDto = new LoginRequestDto(loginId, loginPwd, loginUserType);
+//            System.out.println(loginUserType);
+//
+//            try {
+//                Response<Message> loginResponse = httpService.login(loginRequestDto).execute();
+//                Message message=loginResponse.body();
+//                if (message.getRole()== Role.ROLE_OWNER){
+//                    Intent intent = new Intent(getApplicationContext(), OwnerMain.class);
+//                    startActivity(intent);
+//                }
+//                else if (message.getRole()== Role.ROLE_USER){
+//                    Intent intent = new Intent(getApplicationContext(), UserMain.class);
+//                    startActivity(intent);
+//                }
+//            }catch (Exception e){
+//                System.out.println("오류");
+//            }
+//        }
+//    }
+//
+//    public void move_login(View v){
+//        Intent intent = new Intent(getApplicationContext(), Login.class);
+//        startActivity(intent);
+//    }
+//    public void go_signup(View v){
+//        Intent intent = new Intent(getApplicationContext(), SignUp.class);
+//        startActivity(intent);
+//    }
 }
