9fb7ec9885bbd41aff8e91eeb978add991b32e64
ads0070 <gch05610@naver.com>
2021-11-14 20:49:06 +0900

    Modified sign_up design

--- client/app/src/main/java/com/example/myapplication/signup/OwnerSignUpDto.java
+++ client/app/src/main/java/com/example/myapplication/signup/OwnerSignUpDto.java
@@ -9,7 +9,6 @@ public class OwnerSignUpDto {
     private String ownerPwd;
     private String ownerPwd2;
     private String ownerNum; //사업자 번호
-    private String corpNum; //법인 번호
     private String storeName; //가맹점명
     private String storeLoc; //가맹점 위치
     private String ownerName; //가맹점주 이름
@@ -18,14 +17,12 @@ public class OwnerSignUpDto {
 
     public OwnerSignUpDto(String ownerId, String ownerPwd,
                           String ownerPwd2, String ownerNum,
-                          String corpNum, String storeName,
-                          String storeLoc, String ownerName,
-                          String storeHP, Role role) {
+                          String storeName, String storeLoc,
+                          String ownerName, String storeHP, Role role) {
         this.ownerId = ownerId;
         this.ownerPwd = ownerPwd;
         this.ownerPwd2 = ownerPwd2;
         this.ownerNum = ownerNum;
-        this.corpNum = corpNum;
         this.storeName = storeName;
         this.storeLoc = storeLoc;
         this.ownerName = ownerName;
