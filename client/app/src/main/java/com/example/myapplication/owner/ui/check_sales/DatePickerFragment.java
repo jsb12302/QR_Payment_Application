a975b85261b7d3984560d12b2af124cfc755fca8
Bonyeong1998 <bonyoung1998@gmail.com>
2021-11-21 23:15:43 +0900

    날짜 선택 가능 및 Owner 앱바 색상 수정

---
+++ client/app/src/main/java/com/example/myapplication/owner/ui/check_sales/DatePickerFragment.java
@@ -0,0 +1,36 @@
+package com.example.myapplication.owner.ui.check_sales;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.widget.DatePicker;
+
+import com.example.myapplication.owner.OwnerMain;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(),this,year,month,day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        OwnerMain activity = (OwnerMain)getActivity();
+        activity.processDatePickerResult(year,month,day);
+    }
+}
